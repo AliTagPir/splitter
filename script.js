480581eb4613189b198e6ae6ef6af125bcd696fb
alitagpir <alitagpir@gmail.com>
2021-11-01 14:38:08 +0000

    random colours

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 "use strict"
+function split4(){
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+}
 
-alert("hello")
+
+function addQuarter(intoDiv){
+    let q=document.createElement("div")
+    q.style.backgroundColor=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})` 
+    
+    
+    q.classList.add("flexy")
+    q.classList.add("quarter")
+    
+    q.addEventListener("click",split4)
+
+    intoDiv.appendChild(q)
+
+
+
+}
+
+document.body.addEventListener("click",split4)
